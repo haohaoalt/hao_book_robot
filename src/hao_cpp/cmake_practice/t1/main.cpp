bddca6288d32dcc9cfc6907d407b647b77f8afb2
hao007 <haohaoalt@163.com>
2022-09-08 15:13:00 +0800

    change 0908

---
+++ src/hao_cpp/cmake_practice/t1/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Author: zhanghao
+ * @Date: 2022-09-08 13:55:30
+ * @LastEditTime: 2022-09-08 13:56:33
+ * @FilePath: /hao_book_robot/src/hao_cpp/cmake_practice/t1/main.cpp
+ * @Description: 
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hello cmake!" << endl;
+    return 0;
+}
---
+++ src/hao_cpp/cmake_practice/t2/src/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * @Author: zhanghao
+ * @Date: 2022-09-08 14:05:32
+ * @LastEditTime: 2022-09-08 14:05:33
+ * @FilePath: /hao_book_robot/src/hao_cpp/cmake_practice/t2/src/main.cpp
+ * @Description: 
+ */
+/*
+ * @Author: zhanghao
+ * @Date: 2022-09-08 13:55:30
+ * @LastEditTime: 2022-09-08 13:56:33
+ * @FilePath: /hao_book_robot/src/hao_cpp/cmake_practice/t1/main.cpp
+ * @Description: 
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hello cmake!" << endl;
+    return 0;
+}
